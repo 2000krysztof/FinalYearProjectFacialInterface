d437fdeb9b5e0d8db4ab9398078f970724b133d0
2000krysztof <2000krzysztof@gmail.com>
2025-11-18 01:37:19 +0000

    Add 3d model and base shaders, and add code to render it

---
+++ res/shader.frag
@@ -0,0 +1,13 @@
+#version 330 core
+out vec4 fragColor;
+in vec3 fragNormal;
+
+
+void main() {
+	vec3 N = normalize(fragNormal);
+
+    float light = max(dot(N, normalize(vec3(0.4, 0.7, 0.2))), 0.0);
+    float light_two = max(dot(N, normalize(vec3(-0.4, 0.4, 0.2))), 0.0)*0.1;
+
+    fragColor = vec4(vec3(light+light_two), 1.0);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,31 @@
 #include "raylib.h"
-#include <iostream>
 int main(){
 	InitWindow(800, 600, "Facial Interface");
 
     SetTargetFPS(60);
+	Model faceModel = LoadModel("res/FaceModel.glb");
+	int animCount = 0;
+	ModelAnimation* anims = LoadModelAnimations("res/FaceModel.glb", &animCount);
+
+    Camera3D cam = { 0 };
+    cam.position = { 0.0f, 0.1f, 1.0f };
+    cam.target   = { 0.0f, 0.1f, 0.0f };
+    cam.up       = { 0.0f, 1.0f, 0.0f };
+    cam.fovy     = 45.0f;
+    cam.projection = CAMERA_PERSPECTIVE;
+
+	Shader shader = LoadShader("res/shader.vert", "res/shader.frag");
+
+	faceModel.materials[0].shader = shader;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
+		BeginMode3D(cam);
 
+        DrawModel(faceModel, {0,0,0}, 0.1f, WHITE);
+
+        EndMode3D();
 
         EndDrawing();
     }
